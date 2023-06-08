517c750bb1f565b89b623965228bf60ad4e590db
Antoine Villatte <anvil@Antoines-Mac-mini.local>
2023-06-08 10:25:14 -0400

    added limit to main function

--- src/parse.py
+++ src/parse.py
@@ -53,4 +53,4 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--limit', type=int, default=None)
-    main()
+    main(limit=parser.parse_args().limit)
